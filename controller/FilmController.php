8a6511065fc05120cd21b47a86e86fbcac27a4d8
alphaoumarb <alpha.bah98.ab@gmail.com>
2018-05-29 09:56:23 +0200

    ajout genres et relaisateurs

--- controller/FilmController.php
+++ controller/FilmController.php
@@ -4,10 +4,15 @@ class FilmController extends Controller {
     public function display() {
         $slug = $this->route["params"]["slug"];
         $film = Film::getFromSlug($slug);
-        
+        $genres = Film::getGenderFromFilm($film['id_films']);
+        $realisateurs = Film::getRealFromFilm($film['id_films']);
+
+
         $template = $this->twig->loadTemplate('/Film/display.html.twig');
         echo $template->render(array(
-            'film'  => $film
+            'film'  => $film,
+            'genres' => $genres,
+            'realisateurs'=> $realisateurs,
         ));
     }
 }
--- model/Film.php
+++ model/Film.php
@@ -1,7 +1,33 @@
 <?php
 
 class Film extends Model {
-   public $id_films, $titre, $annee, $synopsis, $images;
+   public $id_films, $titre, $annee, $synopsis, $images, $genre, $realisateur;
+    public static function getGenderFromFilm($id_film) {
+$db = Database::getInstance();
+$sql = "SELECT * FROM genres as g
+INNER JOIN films_as_genres as h
+WHERE g.id_genres = h.id_genres
+AND h.id_films = :id_films";
+$stmt = $db->prepare($sql);
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->bindValue(':id_films', $id_film, PDO::PARAM_INT);
+$stmt->execute();
+return $stmt->fetchAll();
+
+    }
+
+public static function getRealFromFilm($id_film) {
+    $db = Database::getInstance();
+    $sql = "SELECT * FROM realisateurs as r
+    INNER JOIN films_as_realisateurs as h
+    WHERE r.id_realisateurs = h.id_realisateurs
+    AND h.id_films = :id_films";
+    $stmt = $db->prepare($sql);
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->bindValue(':id_films', $id_film, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll();
+    }
 
    public static function getFromSlug( $slug ) {
       $db = Database::getInstance();
@@ -13,6 +39,8 @@ class Film extends Model {
       return $stmt->fetch();
    }
 
+
+
    public static function getList() {
       $db = Database::getInstance();
       $sql = "SELECT * FROM films order by rand() limit 3";
@@ -23,6 +51,8 @@ class Film extends Model {
    }
 
 }
+
+
 
 
 
